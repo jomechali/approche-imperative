e0da1064b096bab8ed736737b6664f5c9534d474
Joseph MECHALI <mechali.joseph@gmail.com>
2022-06-08 15:05:24 +0200

    first exercises

---
+++ src/fr/declaration/variable/DeclarationApp.java
@@ -0,0 +1,24 @@
+package fr.declaration.variable;
+
+public class DeclarationApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		byte a = 1;
+		short b = 2;
+		int c = 3;
+		float d = 4.0f;
+		double e = 5.0d;
+		char f = 'f';
+		String g = "g";
+		
+		long h = 6L;
+		boolean	i = true;
+		
+		String randomString = "le resultat d un calcul \n: 1+5=6";
+		
+		System.out.println(f);
+
+	}
+
+}
