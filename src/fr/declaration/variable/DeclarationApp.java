7066b858b81ce3d0a7698661363e6cb196af537a
Joseph MECHALI <mechali.joseph@gmail.com>
2022-06-08 16:42:37 +0200

    end declarationApp - begin DeclarationOperateur

--- src/fr/declaration/variable/DeclarationApp.java
+++ src/fr/declaration/variable/DeclarationApp.java
@@ -3,12 +3,13 @@ package fr.declaration.variable;
 public class DeclarationApp {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
 		byte a = 1;
 		short b = 2;
 		int c = 3;
 		float d = 4.0f;
-		double e = 5.0d;
+		double e = 057; //octale, base 8 (donc chiffres de 0 a 7!)
+		//double e = 5.0d;
 		char f = 'f';
 		String g = "g";
 		
@@ -17,7 +18,8 @@ public class DeclarationApp {
 		
 		String randomString = "le resultat d un calcul \n: 1+5=6";
 		
-		System.out.println(f);
+		System.out.println(e);
+
 
 	}
 
