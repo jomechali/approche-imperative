fed897efc51bfd26f112a606d9f2e5b143ad5ac4
Joseph MECHALI <mechali.joseph@gmail.com>
2022-06-09 15:06:29 +0200

    exercise : First Last 6

---
+++ src/fr/algorithmie/FirstLast6.java
@@ -0,0 +1,24 @@
+package fr.algorithmie;
+
+public class FirstLast6 {
+
+	public static void main(String[] args) {
+
+		//int[] array1 = {1, 15, -3, 8, 7, 4, -2, 28, -1, 17, 2, 3, 0, 14, -4}; // false
+		int[] array1 = {1, 15, -3, 8, 7, 4, -2, 28, -1, 17, 2, 3, 0, 14, 6}; // true
+		int array1Count = array1.length;
+		
+		boolean control = false;
+		
+		if (array1Count == 1 
+				|| 
+				(array1Count != 0 
+				&& (array1[0] == 6 || array1[array1Count - 1] == 6))) {
+			
+			control = true;
+		}
+		
+		System.out.println("the control bool is : " + control);
+	}
+
+}
