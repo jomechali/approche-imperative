d57a2b4ac0764770cd6ce11a34a0b8be9ddd243f
Joseph MECHALI <mechali.joseph@gmail.com>
2022-06-09 15:08:41 +0200

    exercise : First Last 6 with tests

--- src/fr/algorithmie/FirstLast6.java
+++ src/fr/algorithmie/FirstLast6.java
@@ -5,7 +5,9 @@ public class FirstLast6 {
 	public static void main(String[] args) {
 
 		//int[] array1 = {1, 15, -3, 8, 7, 4, -2, 28, -1, 17, 2, 3, 0, 14, -4}; // false
-		int[] array1 = {1, 15, -3, 8, 7, 4, -2, 28, -1, 17, 2, 3, 0, 14, 6}; // true
+		//int[] array1 = {1, 15, -3, 8, 7, 4, -2, 28, -1, 17, 2, 3, 0, 14, 6}; // true
+		//int[] array1 = new int[0]; // false
+		int[] array1 = new int[1]; // true
 		int array1Count = array1.length;
 		
 		boolean control = false;
