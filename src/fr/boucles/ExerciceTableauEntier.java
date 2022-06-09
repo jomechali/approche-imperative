d8aee16b43ba11f76139659bcf4a7ece1d821004
Joseph MECHALI <mechali.joseph@gmail.com>
2022-06-09 10:56:20 +0200

    finished exercise: Exercice tableau entier

--- src/fr/boucles/ExerciceTableauEntier.java
+++ src/fr/boucles/ExerciceTableauEntier.java
@@ -3,8 +3,17 @@ package fr.boucles;
 public class ExerciceTableauEntier {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		int[] firstIntegers = new int[10];
+		
+		for (int i = 0; i < firstIntegers.length; i++) {
+			firstIntegers[i] = i + 1;
+		}
+
+		System.out.println(firstIntegers[0]);
+		System.out.println(firstIntegers.length);
+		System.out.println(firstIntegers[firstIntegers.length - 1]);
+		firstIntegers[4] = 8;
 	}
 
 }
